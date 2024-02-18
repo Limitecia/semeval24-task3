359d9eee942328836bce9a7596bedd66e42b3de6
anaezquerro <anaezquerro0701@gmail.com>
2024-02-18 02:05:17 +0100

    removed cached elements

--- copy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-scp *.py llama:~/seval24-task3/
-scp -r modules llama:~/seval24-task3/
-scp -r data llama:~/seval24-task3/
-scp -r subtask1 llama:~/seval24-task3/
-scp -r subtask2 llama:~/seval24-task3/
-scp -r utils2 llama:~/seval24-task3/
-

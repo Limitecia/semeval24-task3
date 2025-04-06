c2dd05ee22c1691bb04a2e8776a6a7209233178d
LIMITECIA <103173023+Limitecia@users.noreply.github.com>
2025-04-06 17:36:03 +0800

    Update subtask1.py

--- subtask1.py
+++ subtask1.py
@@ -3,14 +3,14 @@ from data import Subtask1Dataset
 from utils import Config
 from argparse import ArgumentParser
 from configparser import ConfigParser
+import torch
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Textual Emotion Cause Analysis')
     parser.add_argument('--conf', type=str, default='config/subtask1.ini', help='Configuration file.')
     parser.add_argument('--path', type=str, default='results/subtask1/', help='Path to load the analyzer or store it.')
-    parser.add_argument('--load', action='store_true', help='Wheter to load the analyzer.')
+    parser.add_argument('--load', action='store_true', help='Whether to load the analyzer.')
     parser.add_argument('--batch_size', type=int, default=1500, help='Batch size.')
-    
     
     modes = parser.add_subparsers(dest='mode')
     train = modes.add_parser('train')
@@ -36,21 +36,30 @@ if __name__ == '__main__':
     conf = ConfigParser()
     conf.read(args.conf)
 
+    # 自动选择设备
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
     if args.load: 
         analyzer = Subtask1Analyzer.load(args.path)
     else:
         assert args.mode == 'train', 'The model needs to be trained'
         analyzer = None
     if args.mode == 'train':
-        train, dev, test = map(Subtask1Dataset.from_path, (args.train, args.dev, args.test))
+        train_data, dev_data, test_data = map(Subtask1Dataset.from_path, (args.train, args.dev, args.test))
         text_conf = Config.from_ini(conf['text'])
         model_conf = Config.from_ini(conf['model'])
-        analyzer = Subtask1Analyzer.build(train, text_conf, **model_conf())
-        analyzer.train(train, dev, test, args.path, args.lr, args.epochs, args.batch_size, args.patience) 
-    elif args.mode == 'predict':
-        input = Subtask1Dataset.from_path(args.input)
-        analyzer.predict(input, args.output, args.batch_size)
-    elif args.mode == 'eval':
-        data = Subtask1Dataset.from_path(args.data)
-        analyzer.eval(data, '/tmp/pred.json', '/tmp/gold.json', args.batch_size)
+        analyzer = Subtask1Analyzer.build(train_data, text_conf, **model_conf())
         
+        # 将模型移动到指定设备
+        analyzer.model.to(device)
+        # 如果检测到多个GPU，则使用DataParallel包装模型
+        if torch.cuda.device_count() > 1:
+            analyzer.model = torch.nn.DataParallel(analyzer.model)
+        
+        analyzer.train(train_data, dev_data, test_data, args.path, args.lr, args.epochs, args.batch_size, args.patience) 
+    elif args.mode == 'predict':
+        input_data = Subtask1Dataset.from_path(args.input)
+        analyzer.predict(input_data, args.output, args.batch_size)
+    elif args.mode == 'eval':
+        eval_data = Subtask1Dataset.from_path(args.data)
+        analyzer.eval(eval_data, '/tmp/pred.json', '/tmp/gold.json', args.batch_size)
